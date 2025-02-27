a881397124cc25255fbb57f2e9e6981edc7c91ff
nickicn <nicolelopes0008@gmail.com>
2025-02-27 10:48:48 -0300

    terminado o index

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -5,22 +5,42 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{
       tabBarActiveTintColor: "#ffd33d",
+      headerStyle: {
+        backgroundColor: "#25292e",
+      },
+      headerShadowVisible: false,
+      headerTintColor: "#fff",
+      tabBarStyle: {
+        backgroundColor: "#25292e",
+      },
     }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color, focused }) => {
+          tabBarIcon: ({ color, focused }) => (
             <Ionicons
               name={focused ? "home-sharp" : "home-outline"}
               color={color}
               size={24}
             />
-          }
+          )
         }}
       />
-      <Tabs.Screen name="about" options={{ title: "Sobre" }} />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "Sobre",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "information-circle" : "information-circle-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
