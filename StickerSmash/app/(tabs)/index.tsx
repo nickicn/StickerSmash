95883d7c3eff7572b8940db9d7843b86c2f642a8
nickicn <nicolelopes0008@gmail.com>
2025-03-20 10:46:44 -0300

    escrever absoluta dá problema no código todo :)

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
-import { FooterComponent } from "react-native-screens/lib/typescript/components/ScreenFooter";
 import * as ImagePicker from 'expo-image-picker';
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -21,11 +22,23 @@ export default function Index() {
     });
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true);
     } else{
       alert("Você não selecionou nenhuma imagem!");
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  }
+
+  const onAddSticker = () => {
+
+  }
+
+  const onSaveImageAsync = () => {
+
+  }
 
   return (
     <View style={styles.container}>
@@ -35,7 +48,13 @@ export default function Index() {
           selectedImage={selectedImage} />
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionRow}>
+            <IconButton icon="refresh" label="Resetar" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Salvar" onPress={onSaveImageAsync} />
+          </View>
+        </View>
       ) : (
         <View style={styles.footerContainer}>
           <Button
@@ -66,4 +85,12 @@ const styles = StyleSheet.create({
     flex: 1 / 3,
     alignItems: "center",
   },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  }
 });
